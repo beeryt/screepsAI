09d4532e5dc089768335fbc75149a10bd6245bf1
Beery, Carl <carl.beery@stryker.com>
2019-05-01 14:56:40 -0700

    Sovereign WIP

--- src/Colony.js
+++ src/Colony.js
@@ -53,7 +53,7 @@ class Colony
       }
     }
 
-    example_graph = new SimpleGraph()
+    let example_graph = new SimpleGraph()
     example_graph.edges = {
       'A': ['B'],
       'B': ['A', 'C', 'D'],
