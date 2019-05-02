a1044a21893e2899cc9187bade11e364ad6b961b
Beery, Carl <carl.beery@stryker.com>
2019-05-02 09:52:16 -0700

    Add floyd_warshall algorithm

--- src/Colony.js
+++ src/Colony.js
@@ -1,5 +1,65 @@
 var Mine = require("Mine");
 
+const floyd_warshall = graph =>
+{
+  let V = 50*50;
+
+  // let dist be a |V| x |V| array of minimum distances initialized to ∞ (undefined)
+  let dist = []
+  for (let i = 0; i < V; ++i) { dist.push(new Array(V)); }
+
+  /*
+   * for each edge (u,v)
+   *   dist[u][v] ← w(u,v) // the weight of the edge (u,v)
+   * for each vertex v
+   *   dist[v][v] ← 0
+   */
+  for (let v = 0; v < V; ++v)
+  {
+    let vx = Math.floor(v/50);
+    let vy = Math.floor(v%50);
+    for (let i = 0; i < 9; ++i)
+    {
+      let ux = Math.floor(i/3) + vx - 1;
+      let uy = Math.floor(i%3) + vy - 1;
+      let u = ux * 3 + uy;
+      if (u == v)
+      {
+        dist[u][v] = 0;
+      } else {
+        dist[u][v] = weight(ux,uy);
+      }
+    }
+  }
+
+  /*
+   * for k from 1 to |V|
+   *   for i from 1 to |V|
+   *     for j from 1 to |V|
+   *       if dist[i][j] > dist[i][k] + dist[k][j]
+   *         dist[i][j] ← dist[i][k] + dist[k][j]
+   *       end if
+   */
+  for (let k = 0; k < V; ++k)
+  {
+    for (let i = 0; i < V; ++i)
+    {
+      for (let j = 0; j < V; ++j)
+      {
+        let cost = dist[i][k] + dist[k][j];
+        if (dist[i][j] > cost)
+        {
+          dist[i][j] = cost;
+        }
+      }
+    }
+  }
+
+  return dist;
+};
+
+function getMineMap(mine){}
+
 class Colony
 {
   constructor(room)
@@ -31,7 +91,9 @@ class Colony
     this.pos = this.room.getPositionAt(sumX/mass, sumY/mass);
     this.room.visual.circle(this.pos);
 
-    this.mines.forEach(function(mine) {
+    this.room.memory.mineMap = {};
+    this.mines.forEach(mine => {
+      this.room.memory.mineMap[mine] = getMineMap(mine);
       mine.init();
     });
   }
