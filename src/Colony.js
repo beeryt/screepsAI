3836233a54eff3557acebb03588cdabb9c748a4a
Carl Beery <carlbeery@outlook.com>
2019-05-01 17:51:25 -0700

    WIP

--- src/Colony.js
+++ src/Colony.js
@@ -90,6 +90,7 @@ class Colony
       room.visual.line(avoid, pos)
       let ret = PathFinder.search(pos, {pos: avoid}, {flee: true, maxCost: 20});
       let target = _.find(ret.path, pos => pos.getRangeTo(origin) == 1);
+      room.visual.circle(origin, {fill: "#aaff00"})
       room.visual.circle(target, {radius: 0.5});
       return avoid.getDirectionTo(pos);
 
