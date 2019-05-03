90dd5462c3bc1252dd811e45125aced88354ca75
Carl Beery <carlbeery@outlook.com>
2019-05-02 20:34:18 -0700

    WIP

--- src/Colony.js
+++ src/Colony.js
@@ -100,7 +100,7 @@ const dijkstra_getNeighbors = (u) => {
 
 const dijkstra_length = (dist, prev, u, v) => {
   let length = 0;
-  return Math.floor(Math.random()*255);
+  return Game.rooms['sim'].getTerrain(Math.floor(v/50),v%50);
 }
 
 const dijkstra = (graph, source) => {
