a5a94ba9e25f14e26160d9dbc0a8e9ac700fa828
Carl Beery <carlbeery@outlook.com>
2019-05-02 17:13:17 -0700

    WIP

--- src/Colony.js
+++ src/Colony.js
@@ -89,11 +89,10 @@ const dijkstra_getNeighbors = (u) => {
     let v = x*50 + y;
     neighbors.push[v];
 
-    if (u > 50) continue; // only draw first 50 
-    let room = Game.rooms['sim']
-    let p1 = room.getPositionAt(Math.floor(v/50), v%50);
-    let p2 = room.getPositionAt(Math.floor(u/50), u%50);
-    room.visual.line(p1,p2, {color: "#000"});
+    // let room = Game.rooms['sim']
+    // let p1 = room.getPositionAt(Math.floor(v/50), v%50);
+    // let p2 = room.getPositionAt(Math.floor(u/50), u%50);
+    // room.visual.line(p1,p2, {color: "#000"});
   }
   return neighbors;
 };
