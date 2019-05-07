6c1978b68a5155217f7955ae47f0f54aac947ba6
Carl Beery <carlbeery@outlook.com>
2019-05-06 20:03:08 -0700

    debug

--- src/Colony.js
+++ src/Colony.js
@@ -289,7 +289,7 @@ class Colony
       let p = iToPos(i);
       let cost = this.combined_costs[i];
       let radius = map(cost, 0, 100, 0, 0.45);
-      let colorIndex = 255 - Math.round(map(cost, 90, 160, 0, 255));
+      let colorIndex = 255 - Math.round(map(cost, 90, 115, 0, 255));
       let color = "rgba(255,0," + colorIndex + ", 1)";
       // this.room.visual.circle(iToPos(i), {radius: radius, fill: "#ffaa00"})
       this.room.visual.rect(p.x-0.5,p.y-0.5,1,1, {fill: color, opacity: 2});
