99ab9c77826855711201650817abad5d3acff953
Beery, Carl <carlbeery@outlook.com>
2019-05-14 16:21:46 -0700

    Configure rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,62 +1,36 @@
 "use strict";
 
+import clear from "rollup-plugin-clear";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
-import progress from "rollup-plugin-progress";
+import { terser } from "rollup-plugin-terser";
 import typescript from "rollup-plugin-typescript2";
 import screeps from "rollup-plugin-screeps";
 
 let cfg;
 const dest = process.env.DEST;
-if (!dest) {
-    console.log('\x1b[46m%s\x1b[0m \x1b[36m%s\x1b[0m', 'Compiling TypeScript...', '(deploy destination: none)');
-} else if ((cfg = require("./screeps")[dest]) == null) {
-    throw new Error("Invalid upload destination");
-} else {
-    console.log('\x1b[46m%s\x1b[0m \x1b[36m%s\x1b[0m', 'Compiling TypeScript...', `(deploy destination: ${dest})`);
+if (dest && (cfg = require("./screeps.json")[dest]) == null)
+{
+  throw new Error("Invalid upload destination");
 }
 
-const ignoreWarnings = ['commonjs-proxy',
-                        'Circular dependency',
-                        "The 'this' keyword is equivalent to 'undefined'",
-                        "Use of eval is strongly discouraged"];
+console.log("Compiling...");
+if (dest) console.log(`Deploying ${dest}...`);
 
 export default {
-    input: "src/main.ts",
+  input: "src/main.ts",
+  output: {
+    file: "dist/main.js",
+    format: "cjs",
+    sourcemap: true
+  },
 
-    plugins: [
-        progress({clearLine: true}),
-        resolve(),
-        commonjs({
-                     namedExports: {
-                     }
-                 }),
-        typescript({tsconfig: "./tsconfig.json"}),
-        screeps({config: cfg, dryRun: cfg == null})
-    ],
-
-    onwarn: function (warning) {
-        // Skip default export warnings from using obfuscated overmind file in main
-        for (let ignoreWarning of ignoreWarnings) {
-            if (warning.toString().includes(ignoreWarning)) {
-                return;
-            }
-        }
-        // console.warn everything else
-        console.warn(warning.message);
-    },
-
-    treeshake: false,
-
-    output: {
-        file: "dist/main.js",
-        format: "cjs",
-        sourcemap: false,
-        banner: '//\n' +
-                '// ___________________________________________________________\n' +
-                '// _______________________ Screeps AI ________________________\n' +
-                '//\n' +
-                '//\n'
-    },
-
-}
+  plugins: [
+    clear({ targets: ["dist"] }),
+    resolve(),
+    commonjs(),
+    terser({sourcemap: true}),
+    typescript({tsconfig: "./tsconfig.json"}),
+    screeps({config: cfg, dryRun: cfg == null})
+  ]
+};
