260dbcec75843731ad0c7965bd476e9cdbd4b2f8
Beery, Carl <carlbeery@outlook.com>
2019-05-29 16:00:36 -0700

    Add npm typedoc command

--- src/algorithms/dijkstra.ts
+++ src/algorithms/dijkstra.ts
@@ -1,11 +1,30 @@
 import { FibonacciHeap, INode } from "@tyriar/fibonacci-heap";
 import { OMap, IKey } from "../OMap";
 
+/**
+ * Interface for a weighted graph.
+ * @typeparam V   An object representing a vertex in the graph
+ */
 export interface IGraph<V>
 {
+  /** An iterable list of vertices for the graph. */
   vertices: Iterable<V>;
+  /** An iterable list of edges for the graph. */
   edges: Iterable<[V,V]>;
+
+  /**
+   * Returns the weight of the edge between two vertices
+   * @param u   The starting vertex
+   * @param v   The ending vertex
+   * @returns   number weight of edge or `NaN` if no edge exists
+   */
   weight(u: V, v: V): number;
+
+  /**
+   * Returns the neighboring vertices of `v`.
+   * @param v   The vertex to search
+   * @returns   An iterable list of vertices which neighbor `v`
+   */
   neighbors(v: V): Iterable<V>;
 }
 
